51c333e9baaeef56bb829ae647930b37b9157a9a
kayuksel <46922047+kayuksel@users.noreply.github.com>
2023-01-05 14:09:02 +0100

    Update qc_backtest.py

--- qc_backtest.py
+++ qc_backtest.py
@@ -4,27 +4,29 @@
 
 import numpy as np
 
-syms = ['AAPL', 'MSFT', 'TXN', 'SNPS', 'MCHP', 'NVDA', 'SBUX', 'ASML', 'QCOM', 'APH', 'KLAC', 'CRM', 'GOOGL']
+syms = ['MSFT', 'AAPL', 'TXN', 'ASML', 'ADBE', 'CRM', 'SBUX', 'MCHP', 'MA', 'ADI', 'SWKS', 'SNPS', 'KLAC']
 
 class MultidimensionalModulatedRegulators(QCAlgorithm):
     def Initialize(self):
         self.SetStartDate(2020, 1, 1)
+        #self.SetEndDate(2017, 1, 1)
         self.SetCash(100000)
         self.SetExecution(VolumeWeightedAveragePriceExecutionModel())
  
         self.symbols = []
         for i in range(len(syms)):
             self.symbols.append(Symbol.Create(syms[i], SecurityType.Equity, Market.USA))
+            self.Debug(syms[i])
             
         self.SetUniverseSelection(ManualUniverseSelectionModel(self.symbols) )
         self.UniverseSettings.Resolution = Resolution.Hour
         
         self.AddEquity('VGT', Resolution.Hour)
         self.SetBenchmark('VGT')
-
+        
         self.SetBrokerageModel(AlphaStreamsBrokerageModel())
 
-        self.constant_weights = np.array([0.22184507, 0.17495915, 0.13058954, 0.1220931 , 0.07822578, 0.07674023, 0.06398375, 0.04677756, 0.02314778, 0.02249317, 0.0162905 , 0.012325  , 0.01052937])
+        self.constant_weights = np.array([0.28535828, 0.23993727, 0.12635303, 0.05360154, 0.04681825, 0.04518177, 0.04505873, 0.04194283, 0.03757035, 0.03131177, 0.02168459, 0.0151392 , 0.01004238])
         self.constant_weights = self.constant_weights / np.sum(np.abs(self.constant_weights))
 
     def OnData(self, data):
